484b055631293b72a6bf68813ba5916a9e8793f5
KODIPAKA SAIKRUPA <60288247+saikrupa047@users.noreply.github.com>
2022-05-13 11:42:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("my parents new healthy diner!")
+streamlit.title('my parents new healthy diner!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega & bluberry Oatmeal')
+streamlit.text('kale, spinach and rocket smooothie')
+streamlit.text('hard-boiled free-range egg')

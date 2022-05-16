7db1337f6b80e4fc9ee6f1cae35b4e8160bb3b08
KODIPAKA SAIKRUPA <60288247+saikrupa047@users.noreply.github.com>
2022-05-16 16:22:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,6 @@ import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized=pandas.json_normalize(fruityvice_json.json())
+streamlit.dataframe(fruityvice_normalized)
